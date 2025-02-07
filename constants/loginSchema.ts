d7bb23d4561423b17ea5a9ba01d721d5f439e0fb
김보미 <kimbomi2172@naver.com>
2025-02-07 22:35:31 +0900

    ♻️ refactor: 스키마 재설정

--- constants/loginSchema.ts
+++ constants/loginSchema.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.string().email('유효한 이메일을 입력해주세요.'),
-  password: z.string().min(6, '비밀번호는 최소 6자 이상 입력해야 합니다.'),
+  email: z
+    .string()
+    .email('유효한 이메일을 입력해주세요.')
+    .min(1, '이메일은 필수입니다.'),
+  password: z
+    .string()
+    .min(8, '8자 이상 입력해주세요.')
+    .regex(/[a-z]/, '소문자가 포함되어야 합니다.')
+    .regex(/[0-9]/, '숫자가 포함되어야 합니다.')
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, '특수문자가 포함되어야 합니다.')
+    .regex(/^\S*$/, '공백을 포함할 수 없습니다.')
+    .min(1, '비밀번호는 필수입니다.'),
 })
--- constants/signupSchema.ts
+++ constants/signupSchema.ts
@@ -2,9 +2,22 @@ import { z } from 'zod'
 
 export const signupSchema = z
   .object({
-    nickname: z.string().min(2, '닉네임은 최소 2자 이상 입력해주세요.'),
-    email: z.string().email('유효한 이메일을 입력해주세요.'),
-    password: z.string().min(6, '비밀번호는 최소 6자 이상 입력해야 합니다.'),
+    nickname: z
+      .string()
+      .max(10, '열 자 이하로 작성해주세요.')
+      .min(1, '닉네임은 필수입니다.'),
+    email: z
+      .string()
+      .email('유효한 이메일을 입력해주세요.')
+      .min(1, '이메일은 필수입니다.'),
+    password: z
+      .string()
+      .min(8, '8자 이상 입력해주세요.')
+      .regex(/[a-z]/, '소문자가 포함되어야 합니다.')
+      .regex(/[0-9]/, '숫자가 포함되어야 합니다.')
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, '특수문자가 포함되어야 합니다.')
+      .regex(/^\S*$/, '공백을 포함할 수 없습니다.')
+      .min(1, '비밀번호는 필수입니다.'),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
