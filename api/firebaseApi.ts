27c93abde137e6adb276b7232aa22c7718ddea3d
김보미 <kimbomi2172@naver.com>
2025-02-07 22:04:23 +0900

    ♻️ refactor: firebase auth 연동

--- api/firebaseApi.ts
+++ api/firebaseApi.ts
@@ -1,8 +1,27 @@
 import { FinalSignupFormData } from '@/app/(auth)/signup'
-import { db } from '@/firebaseConfig'
+import { db, auth } from '@/firebaseConfig'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { addDoc, collection } from 'firebase/firestore'
 
 // 유저 추가
 export const addUser = async (userData: FinalSignupFormData): Promise<void> => {
-  await addDoc(collection(db, 'users'), userData)
+  try {
+    // Firebase Authentication에 유저 등록
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      userData.email,
+      userData.password
+    )
+    const user = userCredential.user
+
+    // Firestore에 유저 정보 저장
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      nickname: userData.nickname,
+      email: userData.email,
+    })
+  } catch (error) {
+    console.error('회원가입 실패:', error)
+    throw error
+  }
 }
