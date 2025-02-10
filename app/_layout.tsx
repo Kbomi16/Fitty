606995c88936fdfd80311c8cc0de21f218a21663
김보미 <kimbomi2172@naver.com>
2025-02-10 17:07:31 +0900

    🔥 remove: react query devtools 삭제

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { queryClient } from '@/queryClient'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import { Image } from 'react-native'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function RootLayout() {
   return (
@@ -26,7 +25,6 @@ export default function RootLayout() {
         }}
       >
         <Stack.Screen name="(tabs)/home" options={{ headerShown: false }} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </Stack>
     </QueryClientProvider>
   )
