9eba8358f9749b55fa38d414ccf581c9172800c0
김보미 <kimbomi2172@naver.com>
2025-02-11 14:07:08 +0900

    💄 style: 검색 컴포넌트 구현

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,34 +2,45 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import * as Location from 'expo-location'
 import KakaoMap from '@/components/KakaoMap'
+import SearchGym from '@/components/searchGym'
 
 export default function Home() {
   const [location, setLocation] = useState<{
     latitude: number
     longitude: number
   } | null>(null)
+  const [searchResult, setSearchResult] = useState<{
+    latitude: number
+    longitude: number
+  } | null>(null)
+
+  // 현재 위치 가져오기
+  const getCurrentLocation = async () => {
+    try {
+      const { coords } = await Location.getCurrentPositionAsync({})
+      setLocation({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      })
+    } catch (error) {
+      console.error('위치 정보를 가져오는 데 실패했습니다:', error)
+    }
+  }
 
   useEffect(() => {
-    const getCurrentLocation = async () => {
-      // 현재 위치 가져오기
-      try {
-        const { coords } = await Location.getCurrentPositionAsync({})
-        setLocation({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-        })
-      } catch (error) {
-        console.error('위치 정보를 가져오는 데 실패했습니다:', error)
-      }
-    }
-
     getCurrentLocation()
   }, [])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>안녕하세요!</Text>
-      {location ? (
+      <Text style={styles.text}>헬스장을 등록해주세요!</Text>
+      <SearchGym location={location} onSearchResult={setSearchResult} />
+      {searchResult ? (
+        <KakaoMap
+          latitude={searchResult.latitude}
+          longitude={searchResult.longitude}
+        />
+      ) : location ? (
         <KakaoMap latitude={location.latitude} longitude={location.longitude} />
       ) : (
         <Text>위치를 가져오는 중입니다...</Text>
@@ -46,6 +57,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#f8f9fd',
   },
   text: {
-    fontSize: 24,
+    fontSize: 20,
+    marginBottom: 10,
   },
 })
