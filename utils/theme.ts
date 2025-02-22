6cf54744e6f3d4a8352c0eb5161ce9e9dd6b87e0
김보미 <kimbomi2172@naver.com>
2025-02-22 22:55:35 +0900

    ✨ feat: 테마 context 생성

---
+++ utils/theme.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  background: '#f8f9fd',
+  text: '#000',
+  secondaryText: '#457b9d',
+}
+
+export const darkTheme = {
+  background: '#1c1c1e',
+  text: '#fff',
+  secondaryText: '#a0aec0',
+}
+
+export type ThemeType = typeof lightTheme
